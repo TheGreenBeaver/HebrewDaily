d65630e44374a21bf8fa9196522e8791feb7b2ac
TheGreenBeaver <leonkhvatskin@gmail.com>
2023-04-23 20:54:42 +0300

    help description

--- src/engine/tg/routing/misc.ts
+++ src/engine/tg/routing/misc.ts
@@ -1,14 +1,29 @@
-/* eslint-disable max-len */
 import { Composer } from 'grammy';
 
 import type { EnhancedContext } from '../types';
 
 export const composer = new Composer<EnhancedContext>();
 
+const Desc = {
+  translate:
+    'перевести с иврита на русский, пользуясь <a href="https://www.slovar.co.il">словарём Ирис</a>',
+  googleAuth:
+    'авторизоваться в Google; это нужно, чтобы я мог получить доступ' +
+    ' к Google Classroom и обработать следующую команду',
+  listWords:
+    'перечислить слова, изученные на занятиях, с переводом' +
+    ' (непосредственно в сообщении - только один вариант перевода,' +
+    ' в прикреплённом PDF-файле - полная информация из словаря)',
+};
+
 composer.command('help', ctx => ctx.reply(`
 Шалом! Вот список команд, которые я умею обрабатывать:
-- <b>/translate</b>: перевести с иврита на русский, пользуясь <a href="https://www.slovar.co.il">словарём Ирис</a>
+- <b>/translate</b>: ${Desc.translate}
 
-- <b>/googleAuth</b>: авторизоваться в Google; это нужно, чтобы я мог получить доступ к Google Classroom и обработать следующую команду
-- <b>/listWords</b>: вывести список всех слов, которые Вы успели изучить на занятиях
+- <b>/googleAuth</b>: ${Desc.googleAuth}
+- <b>/listWords</b>: ${Desc.listWords}
+  - Просто <code>/listWords</code>: данные по последнему занятию
+  - <code>/listWords all</code>: данные по всем прошедшим занятиям
+  - <code>/listWords 04/04/23</code>: данные по занятию от 4 апреля
+  - <code>/listWords 04/04/23 - 10/04/23</code>: данные по занятиям с 4 по 10 апреля
 `, { parse_mode: 'HTML' }));
