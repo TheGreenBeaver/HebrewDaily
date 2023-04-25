863919b73ddfa36a720c956bd85468d7fb091c74
TheGreenBeaver <leonkhvatskin@gmail.com>
2023-04-25 17:19:55 +0300

    - snake-case commands - systemctl support

--- src/engine/tg/routing/google-services/constants.ts
+++ src/engine/tg/routing/google-services/constants.ts
@@ -1,4 +1,4 @@
 export enum Commands {
-  listWords = 'listWords',
-  googleAuth = 'googleAuth',
+  listWords = 'list_words',
+  googleAuth = 'google_auth',
 }
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+cd "$BOT_ROOT" && yarn start
